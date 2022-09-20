e3c4c6b2e54649634b795debcbbb9c9132b45f38
Skitten168 <kittens@mail.nmc.edu>
2022-09-20 17:53:56 -0400

    Initial commit

---
+++ Models/Songs.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace MusicDB.Models
+{
+    public class Songs
+    {
+        public int Id { get; set; }
+        public string SongTitle { get; set; }
+        public string Artist { get; set; }
+        public string Album { get; set; }
+        public string? Genre { get; set; }
+        public string? RecordLabel { get; set; }
+        public int? SongLength { get; set; }
+        public DateTime? ReleaseDate { get; set; }  
+    }
+}
---
+++ Pages/Jason/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MusicDB.Pages.Jason
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
