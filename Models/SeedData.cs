b1e75ca1901630b37b72ef21124dfb6fbe10aecd
Skitten168 <kittens@mail.nmc.edu>
2022-09-22 17:38:59 -0400

    DB changes

---
+++ Models/SeedData.cs
@@ -0,0 +1,60 @@
+﻿using System.Runtime.Intrinsics.X86;
+
+namespace MusicDB.Models
+{
+    public class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new RazorPagesMovieContext(
+                serviceProvider.GetRequiredService<
+                    DbContextOptions<RazorPagesMovieContext>>()))
+            {
+                if (context == null || context.Movie == null)
+                {
+                    throw new ArgumentNullException("Null RazorPagesMovieContext");
+                }
+
+                // Look for any movies.
+                if (context.Movie.Any())
+                {
+                    return;   // DB has been seeded
+                }
+
+                context.Movie.AddRange(
+                    new Song
+                    {
+                        SongTitle = "I Want It That Way",
+                        Artist = "Backstreet Boys",
+                        Album = "Millenium",
+                        Genre = "Pop",
+                        SongLength = DateTime.Parse("3:33"),
+                        ReleaseDate = DateTime.Parse("4-12-1999")
+                    },
+
+                    new Song
+                    {
+                        SongTitle = "It's Gonna Be Me",
+                        Artist = "NSYNC",
+                        Album = "No Strings Attached",
+                        Genre = "Pop",
+                        SongLength = DateTime.Parse("3:11"),
+                        ReleaseDate = DateTime.Parse("2000-06-12")
+                    },
+
+                    new Song
+                    {
+                        SongTitle = "Oops!... I Did it Again",
+                        Artist = "Britney Spears",
+                        Album = "Oops!... I Did it Again",
+                        Genre = "Dance-pop",
+                        SongLength = DateTime.Parse("3:31"),
+                        ReleaseDate = DateTime.Parse("04-11-2000")
+
+                    }
+                );
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ Models/Song.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace MusicDB.Models
+{
+    public class Song
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string SongTitle { get; set; }
+
+        [StringLength(30)]
+        public string Artist { get; set; }
+
+        [StringLength(30)]
+        public string Album { get; set; }
+
+        [StringLength(30)]
+        public string? Genre { get; set; }
+
+        [DataType(DataType.Time)]
+        [RegularExpression(@"^[0-5]?\d:[0-5]\d$")]
+        public DateTime? SongLength { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime? ReleaseDate { get; set; } 
+     }
+}
