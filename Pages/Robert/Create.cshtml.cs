be64788caf695d33fa6e38db3760db12147dcb22
Skitten168 <kittens@mail.nmc.edu>
2022-09-26 19:37:00 -0400

    test commit

---
+++ Pages/Robert/Create.cshtml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using MusicDB.Data;
+using MusicDB.Models;
+
+namespace MusicDB.Pages.Robert
+{
+    public class CreateModel : PageModel
+    {
+        private readonly MusicDB.Data.MusicDBContext _context;
+
+        public CreateModel(MusicDB.Data.MusicDBContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Song Song { get; set; }
+        
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+          if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Song.Add(Song);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
