3027213c68d20f652a9a059ccbd9f9bc6955a138
Jason Kubo <kuboj86@gmail.com>
2022-09-26 21:21:03 -0400

    Jasons Pages

---
+++ Pages/Jason/Details.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MusicDB.Data;
+using MusicDB.Models;
+
+namespace MusicDB.Pages.Jason
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly MusicDB.Data.MusicDBContext _context;
+
+        public DetailsModel(MusicDB.Data.MusicDBContext context)
+        {
+            _context = context;
+        }
+
+      public Song Song { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Song == null)
+            {
+                return NotFound();
+            }
+
+            var song = await _context.Song.FirstOrDefaultAsync(m => m.Id == id);
+            if (song == null)
+            {
+                return NotFound();
+            }
+            else 
+            {
+                Song = song;
+            }
+            return Page();
+        }
+    }
+}
--- Pages/Jason/Index.cshtml.cs
+++ Pages/Jason/Index.cshtml.cs
@@ -1,12 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MusicDB.Data;
+using MusicDB.Models;
 
 namespace MusicDB.Pages.Jason
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly MusicDB.Data.MusicDBContext _context;
+
+        public IndexModel(MusicDB.Data.MusicDBContext context)
         {
+            _context = context;
+        }
+
+        public IList<Song> Song { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Song != null)
+            {
+                Song = await _context.Song.ToListAsync();
+            }
         }
     }
 }
