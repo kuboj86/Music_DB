b676c7c3827d7d5ba70a39823f6772845279de73
Jason Kubo <kuboj86@gmail.com>
2022-10-01 10:05:27 -0400

    Styling changed. LIne added to _layout to allow for styling

--- Pages/Jason/Index.cshtml.cs
+++ Pages/Jason/Index.cshtml.cs
@@ -18,15 +18,59 @@ namespace MusicDB.Pages.Jason
         {
             _context = context;
         }
+        public string SongTitleSort { get; set; }
+        public int ArtistNameSort { get; set; }
+        public int AlbumNameSort { get; set; }
+        public int GenreSort { get; set; }
 
-        public IList<Song> Song { get;set; } = default!;
+        [BindProperty(SupportsGet = true)]
+        public string? SearchString { get; set; }
+        public IList<Song> Songs { get;set; } = default!;
 
-        public async Task OnGetAsync()
+        //public async Task OnGetAsync()
+        //{
+        //    var songs = from s in _context.Song select s;
+
+        //    if (!string.IsNullOrEmpty(SearchString))
+        //    {
+        //        songs = songs.Where(s => s.SongTitle.Contains(SearchString));
+        //    }
+        //    Songs = await songs.ToListAsync();
+        //}
+        public async Task OnGetAsync(string sort)
         {
-            if (_context.Song != null)
+            var songs = from s in _context.Song select s;
+
+            if (!string.IsNullOrEmpty(SearchString))
             {
-                Song = await _context.Song.ToListAsync();
+                songs = songs.Where(s => s.SongTitle.Contains(SearchString));
             }
+            Songs = await songs.ToListAsync();
+
+            // using System;
+            SongTitleSort = String.IsNullOrEmpty(sort) ? "song_title" : "";
+            //DateSort = sort == "Date" ? "date_desc" : "Date";
+
+            IQueryable<Song> Song = from s in _context.Song
+                                    select s;
+
+            switch (sort)
+            {
+                case "song_title":
+                    Song = Song.OrderByDescending(s => s.SongTitle);
+                    break;
+                //case "Date":
+                //    Song = Song.OrderBy(s => s.EnrollmentDate);
+                //    break;
+                //case "date_desc":
+                //    Song = Song.OrderByDescending(s => s.EnrollmentDate);
+                //    break;
+                default:
+                    //Song = Song.OrderBy(s => s.LastName);
+                    break;
+            }
+
+            Songs = await Song.AsNoTracking().ToListAsync();
         }
     }
 }
