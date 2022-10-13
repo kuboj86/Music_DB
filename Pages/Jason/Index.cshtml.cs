6b378dcf44c9ed5ea15f1e8fc408f3ed938af9fc
Jason Kubo <kuboj86@gmail.com>
2022-10-12 23:25:35 -0400

    The best CSS changes

--- Pages/Jason/Index.cshtml.cs
+++ Pages/Jason/Index.cshtml.cs
@@ -12,20 +12,26 @@ namespace MusicDB.Pages.Jason
 {
     public class IndexModel : PageModel
     {
-        private readonly MusicDB.Data.MusicDBContext _context;
+        private readonly MusicDBContext _context;
 
-        public IndexModel(MusicDB.Data.MusicDBContext context)
+        public IndexModel(MusicDBContext context)
         {
             _context = context;
         }
         public string SongTitleSort { get; set; }
-        public int ArtistNameSort { get; set; }
-        public int AlbumNameSort { get; set; }
-        public int GenreSort { get; set; }
+        public string ArtistNameSort { get; set; }
+        public string AlbumNameSort { get; set; }
+        public string GenreSort { get; set; }
+        public bool isTitleFiltered { get; set; }
+        public bool isArtistFiltered { get; set; }
+        public bool isAlbumFiltered { get; set; }
+
+        public bool isGenreFiltered { get; set; }
+
 
         [BindProperty(SupportsGet = true)]
         public string? SearchString { get; set; }
-        public IList<Song> Songs { get;set; } = default!;
+        public IList<Song> Songs { get; set; } = default!;
 
         //public async Task OnGetAsync()
         //{
@@ -39,38 +45,94 @@ namespace MusicDB.Pages.Jason
         //}
         public async Task OnGetAsync(string sort)
         {
-            var songs = from s in _context.Song select s;
-
-            if (!string.IsNullOrEmpty(SearchString))
-            {
-                songs = songs.Where(s => s.SongTitle.Contains(SearchString));
-            }
-            Songs = await songs.ToListAsync();
-
-            // using System;
-            SongTitleSort = String.IsNullOrEmpty(sort) ? "song_title" : "";
-            //DateSort = sort == "Date" ? "date_desc" : "Date";
-
             IQueryable<Song> Song = from s in _context.Song
                                     select s;
 
+            var songs = from s in _context.Song select s;
+            //SearchString = SearchString;
+
+            // using System;
+            SongTitleSort = String.IsNullOrEmpty(sort) ? "songTitle" : "";
+            ArtistNameSort = String.IsNullOrEmpty(sort) ? "artistName" : "";
+            AlbumNameSort = String.IsNullOrEmpty(sort) ? "albumName" : "";
+            GenreSort = String.IsNullOrEmpty(sort) ? "genreName" : "";
+
             switch (sort)
             {
-                case "song_title":
-                    Song = Song.OrderByDescending(s => s.SongTitle);
+                case "songTitle":
+                    if(isTitleFiltered == false)
+                    {
+                        SetToFalse();
+                        Song = Song.OrderBy(s => s.SongTitle);
+                        isTitleFiltered = true;
+                    }
+                    else
+                    {
+                        Song = Song.OrderByDescending(s => s.SongTitle);
+                        isTitleFiltered = false;
+                    }
                     break;
-                //case "Date":
-                //    Song = Song.OrderBy(s => s.EnrollmentDate);
-                //    break;
-                //case "date_desc":
-                //    Song = Song.OrderByDescending(s => s.EnrollmentDate);
-                //    break;
+
+                case "artistName":
+                    if (isArtistFiltered == false)
+                    {
+                        SetToFalse();
+                        Song = Song.OrderBy(s => s.Artist);
+                        isArtistFiltered = true;
+                    }
+                    else
+                    {
+                        Song = Song.OrderByDescending(s => s.Artist);
+                        isArtistFiltered = false;
+                    }
+                    break;
+                case "albumName":
+                    if (isAlbumFiltered == false)
+                    {
+                        SetToFalse();
+                        Song = Song.OrderBy(s => s.Album);
+                        isAlbumFiltered = true;
+                    }
+                    else
+                    {
+                        Song = Song.OrderByDescending(s => s.Album);
+                        isAlbumFiltered = false;
+                    }
+                    break;
+                case "genreName":
+                    if (isGenreFiltered == false)
+                    {
+                        SetToFalse();
+                        Song = Song.OrderBy(s => s.Genre);
+                        isGenreFiltered = true;
+                    }
+                    else
+                    {
+                        Song = Song.OrderByDescending(s => s.Genre);
+                        isGenreFiltered = false;
+                    }
+                    break;
                 default:
-                    //Song = Song.OrderBy(s => s.LastName);
                     break;
+            }
+
+            if (!String.IsNullOrEmpty(SearchString))
+            {
+                Song = Song.Where(s => s.SongTitle.Contains(SearchString)
+                                       || s.Artist.Contains(SearchString)
+                                       || s.Genre.Contains(SearchString)
+                                       || s.Album.Contains(SearchString));
             }
 
             Songs = await Song.AsNoTracking().ToListAsync();
         }
+        public void SetToFalse()
+        {
+            isTitleFiltered = false;
+            isAlbumFiltered = false;
+            isArtistFiltered = false;
+            isGenreFiltered = false;
+        }
+
     }
 }
